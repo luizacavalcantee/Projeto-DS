7a499c63a66bc6e22a1eddb29370e60e81c106cb
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-14 13:05:45 -0300

    refactor(Home): remove Button component and simplify layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "components/ui/button";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <Button variant={"default"} size={"default"}>Entrar</Button>
+    <div className="">
     </div>
   );
 }
