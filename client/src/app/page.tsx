3bb3a00f8cf48027e99b67591c312f2e7e98daad
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-13 21:22:15 -0300

    feat(footer): update layout to include Footer component and remove it from Home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,8 @@
 'use client';
-import Footer from "components/footer";
-
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col h-full justify-around items-center">
-      <Footer/>
+    <div className="">
     </div>
   );
 }
