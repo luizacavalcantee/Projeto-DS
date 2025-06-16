4c615a23b51105c5331a941808e699042a9f5600
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-16 12:36:57 -0300

    feat(scoring-guide): update scoring guide styles and improve asset imports

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -5,5 +5,4 @@ import Emoji_people from "./emoji_people.svg"
 import Calendar from "./calendar.svg"
 import Public from "./public.svg"
 
-
 export{ Logo, BoraImpactar, Home_work, Emoji_people, Calendar, Public };
