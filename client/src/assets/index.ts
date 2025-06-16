73aab4f39e3e2220b0d259b61f79009be9254323
SofiRibeiro <sofiasantana168@gmail.com>
2025-06-16 11:57:33 -0300

    feat(scoring-guide): create scoring-guide component

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,4 +1,9 @@
 import Logo from "./logo-prefeitura.svg"
 import BoraImpactar from "./bora-impactar.svg"
+import Home_work from "./home_work.svg"
+import Emoji_people from "./emoji_people.svg"
+import Calendar from "./calendar.svg"
+import Public from "./public.svg"
 
-export{ Logo, BoraImpactar };
+
+export{ Logo, BoraImpactar, Home_work, Emoji_people, Calendar, Public };
