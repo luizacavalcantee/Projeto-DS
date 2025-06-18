7c75941c1aff70baddbc49883211db8e76a47596
analuisafeitosa <analuisafguimaraes@hotmail.com>
2025-06-18 13:36:30 -0300

    feat/join-us: creating join us card

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,18 +1,11 @@
 import { Challenge } from "assets";
 import Card from "components/challenge-card";
+import JoinUs from "components/join-us";
 
 export default function Home() {
   return (
     <div className="flex h-screen justify-center items-center">
-      <Card
-        imageSrc={Challenge}
-        imageAlt="Crianças na horta comunitária"
-        title="Horta Comunitária na Escola"
-        description="Construa uma horta na escola e ensine sobre sustentabilidade."
-        progress={50}
-        link="#"
-        linkLabel="Detalhes"
-      />
+      <JoinUs/>
     </div>
   );
 }
