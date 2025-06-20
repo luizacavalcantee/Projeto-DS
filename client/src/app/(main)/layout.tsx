22a0e2e64d0b85f78da7a94f7170ab03eef29652
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-20 18:51:42 -0300

    refactor: improve layout structure and clean up JoinUs component

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex flex-col min-h-screen w-full">
       <main className="flex-grow">
         {children}
       </main>
--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,13 +1,6 @@
-import Header from "@/components/header";
-
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
-      <Header 
-      buttonText="Sou ONG"
-      secondaryButtonText="Sou Gestor Educacional"
-      showSecondaryButton={true}
-      />
+    <div className="flex flex-col h-screen justify-center items-center">
     </div>
   );
 }
