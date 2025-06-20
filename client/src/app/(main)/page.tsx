1609e545c7ed338efddaa1fd4a56d0b69ff43d8d
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-20 20:35:10 -0300

    refactor(scoring-guide): enhance ScoringGuide component with improved layout and styling

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,6 +1,9 @@
+import ScoringGuide from "@/components/scoring-guide";
+
 export default function Home() {
   return (
     <div className="flex flex-col h-screen justify-center items-center">
+      <ScoringGuide />
     </div>
   );
 }
