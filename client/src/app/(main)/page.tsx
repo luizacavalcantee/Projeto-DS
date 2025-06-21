db5b0440dadb5b499fc6840a2c6ee6a8eb5a10c2
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-20 22:12:31 -0300

    feat(title): add Title component with back navigation and page title display

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,9 +1,6 @@
-import ScoringGuide from "@/components/scoring-guide";
-
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <ScoringGuide />
+    <div className="flex flex-col h-screen mt-20">
     </div>
   );
 }
