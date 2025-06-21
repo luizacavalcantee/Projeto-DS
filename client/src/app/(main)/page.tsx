a7ccc9a8421b502fe6e7e8fa4c08513a41478c10
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-21 00:42:47 -0300

    feat(impact): add ImpactSection component with images and descriptive text

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen mt-20 items-center justify-center gap-32 p-32">
+    <div className="flex h-screen mt-20 items-center justify-center">
     </div>
   );
 }
--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -11,6 +11,10 @@ import Trophy from './trophy.svg';
 import School from './school.png';
 import Artesenato from './artesanato.svg';
 import UserProfile from './user.svg';
+import Grid1 from './grid-1.png';
+import Grid2 from './grid-2.png';
+import Grid3 from './grid-3.png';
+import Underline from './underlined.svg';
 
 export {
   Logo,
@@ -25,5 +29,9 @@ export {
   School,
   Challenge,
   Artesenato,
-  UserProfile
+  UserProfile,
+  Grid1,
+  Grid2,
+  Grid3,
+  Underline
 };
