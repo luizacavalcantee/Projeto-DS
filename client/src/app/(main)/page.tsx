ceb9beb897c711a661a34cc5f740c76205a98fa2
analuisafeitosa <analuisafguimaraes@hotmail.com>
2025-06-22 17:01:28 -0300

    feat/checkpoint-modal: creating a modal for the checkpoints/updates

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,6 +1,9 @@
+import CheckpointModal from "@/components/checkpoint-modal";
+
 export default function Home() {
   return (
-    <div className="flex h-screen mt-20 items-center justify-center">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <CheckpointModal buttonText="Checkpoint 1"/>
     </div>
   );
 }
