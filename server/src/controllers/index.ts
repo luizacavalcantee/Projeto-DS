4a279863f9d89542e6b20cd204ef01881022c450
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-24 21:58:32 -0300

    feat(crud): Created Ong model in DBML schema and Prisma schema.

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,5 +1,6 @@
 import UserController from './UserController';
 import LoginController from './LoginController';
 import FileController from './fileController';
+import OngController from './OngController';
 
-export { UserController, LoginController, FileController };
+export { UserController, LoginController, FileController, OngController };
