42d64cfd2d4e07c6efe07b6fda2da98b54376247
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-06-30 11:50:03 -0300

    feat(chellenges): update UI components for improved layout and localization; adjust padding in Title component and translate pagination labels to Portuguese

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,18 +1,6 @@
-"use client"
-
-import { NewButton } from "@/components/ui/new-button";
-import { DeleteModal } from "@/components/delete-modal"
-import React from "react";
-
-
-
-
 export default function Home() {
   return (
     <div className="flex h-screen mt-20">
-       <DeleteModal onConfirm={() => console.log("Exclusão confirmada")}>
-      <button className="">🗑️ Excluir</button>
-      </DeleteModal>
     </div>
   );
 }
