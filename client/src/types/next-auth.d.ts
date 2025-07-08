80add15bf28d11af6926d7e9dcbe53afd713ca77
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-07-08 14:27:07 -0300

    refactor(types): enhance next-auth types for User and Session interfaces

--- client/src/types/next-auth.d.ts
+++ client/src/types/next-auth.d.ts
@@ -1,18 +1,21 @@
-import 'next-auth';
-import { DefaultSession } from 'next-auth';
+import { DefaultSession, DefaultUser } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 
 declare module 'next-auth' {
   interface User {
-    role?: string;
+    id: string;
+    name: string;
+    email: string;
+    role: 'ong';
   }
 
   interface Session {
-    user?: User;
+    user: User;
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    user?: User;
+    user: User;
   }
 }
