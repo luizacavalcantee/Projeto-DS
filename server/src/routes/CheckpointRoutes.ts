6e419f4668050561d97da4f2488dd202c92fc5cf
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-07-11 00:45:31 -0300

    feat: add database schema and migration files for User, Ong, SchoolManager, Challenge, and Checkpoint tables; update CheckpointRoutes

--- server/src/routes/CheckpointRoutes.ts
+++ server/src/routes/CheckpointRoutes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { CheckpointController } from '@controllers';
-// import { UpdateCheckpoint } from '@dtos';
 
 const checkpointRouter = Router();
 
-// A validação será de responsabilidade do controller
 checkpointRouter.route('/:id')
-    .patch(CheckpointController.update) // Sem o middleware validate
+    .patch(CheckpointController.update)
     .get(CheckpointController.readById);
 
 export default checkpointRouter;
