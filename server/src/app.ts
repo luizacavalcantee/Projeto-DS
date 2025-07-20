598a60390d88f73356792d603bc7e1c7a4922ce9
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-07-20 11:22:03 -0300

    fix: update CORS configuration to allow requests from the production URL

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://projeto-ds-nine.vercel.app'],
   }),
 );
 app.use(cookieParser());
